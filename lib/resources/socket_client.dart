2d2fa5efbf9065007e3a9e182ca162b94eea9075
TUSHAR MISHRA <109910958+MISHRA-TUSHAR@users.noreply.github.com>
2024-05-17 23:28:49 +0530

    .

--- lib/resources/socket_client.dart
+++ lib/resources/socket_client.dart
@@ -5,7 +5,7 @@ class SocketClient {
   static SocketClient? _instance;
 
   SocketClient._internal() {
-    socket = IO.io('http://192.168.1.19:3000', <String, dynamic>{
+    socket = IO.io('http://ipaddr:3000', <String, dynamic>{
       'transports': ['websocket'],
       'autoConnect': false,
     });
